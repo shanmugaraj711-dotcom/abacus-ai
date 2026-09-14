520aa4872b2f11d502c298d0f88a829f45f2a818
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-09-14 08:28:31 +0530

    Cache sound system for offline play

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='abacus-ai-v3';
-const ASSETS=['./','./index.html','./styles.css','./tutor.css','./challengeApp.js','./abacusEngine.js','./manifest.json','./assets/mascot/babi.svg','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='abacus-ai-v4';
+const ASSETS=['./','./index.html','./styles.css','./tutor.css','./challengeApp.js','./abacusEngine.js','./sound.js','./manifest.json','./assets/mascot/babi.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
