35c2c9ac81fc40c41d399400a0bb6a8b14f1a19c
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-09-14 17:44:36 +0530

    Create Learning five-bead module

---
+++ learning/02-five-bead.js
@@ -0,0 +1,10 @@
+export const FIVE_BEAD = [
+  {
+    id: 3,
+    title: 'Meet 5',
+    text: 'The upper bead is worth five.',
+    task: 'Tap the upper bead to make 5.',
+    target: 5,
+    note: 'Meet Babi’s special 5 bead.'
+  }
+];
