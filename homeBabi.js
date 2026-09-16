c3d0e46443e4adf33d9316de3917c94f2f19a2b2
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-09-16 07:34:40 +0530

    Add Home Babi pose compositor

---
+++ homeBabi.js
@@ -0,0 +1,17 @@
+(function(){
+  const POSES={idle:'./assets/mascot/babi-idle.svg',pointing:'./assets/mascot/babi-pointing.svg',celebrating:'./assets/mascot/babi-celebrating.svg',encouraging:'./assets/mascot/babi-encouraging.svg',teaching:'./assets/mascot/babi-teaching.svg'};
+  function apply(){
+    const host=document.querySelector('.world-hero .world-babi');
+    if(!host||host.dataset.poseReady)return;
+    host.dataset.poseReady='1';
+    host.innerHTML='<img class="babi babi-small babi-home-pointing" src="'+POSES.pointing+'" alt="Babi pointing the way">';
+    const mission=document.querySelector('.mission');
+    if(mission&&!mission.querySelector('.mission-babi')){
+      const img=document.createElement('img');img.className='mission-babi';img.src=POSES.encouraging;img.alt='Babi encouraging you';mission.prepend(img);
+    }
+  }
+  const app=document.querySelector('#app');
+  if(!app)return;
+  apply();
+  new MutationObserver(apply).observe(app,{childList:true,subtree:true});
+})();
